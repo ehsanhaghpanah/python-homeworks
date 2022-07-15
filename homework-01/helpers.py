8d6308cf7207395051ccde26ba3694a7c1a79fb7
Ehsan Haghpanah <anonim.developer@gmail.com>
2022-07-15 23:40:47 +0430

    .

---
+++ homework-01/helpers.py
@@ -0,0 +1,48 @@
+#
+# (C) Ehsan Haghpanah, 2022.
+# haghpanah@scenus.com
+#
+
+from multipledispatch import dispatch
+
+#
+# tries to parse provided string as an integer
+#
+# @param text : a string representing an integer number
+#
+def try_parse(text: str) -> int:
+    try:
+        return int(text)
+    except:
+        return None
+
+#
+# tries to get an integer number from input
+#
+@dispatch()
+def get_int() -> int:
+    return get_int("Enter an Integer Number = ", "ERROR: Not a Valid Integer Number, Returning None")
+
+#
+# tries to get an integer number from input
+#
+# @param i_msg : input function prompt message
+#
+@dispatch(str)
+def get_int(i_msg: str) -> int:
+    return get_int(i_msg, "ERROR: Not a Valid Integer Number, Returning None")
+
+#
+# tries to get an integer number from input
+#
+# @param i_msg : input function prompt message
+# @param e_msg : error message
+#
+@dispatch(str, str)
+def get_int(i_msg: str, e_msg: str) -> int:
+
+    rs = try_parse(input(i_msg))
+    if (rs == None):
+        print(e_msg)
+
+    return rs
--- homework-01/main.py
+++ homework-01/main.py
@@ -1,2 +1,45 @@
+#
+# (C) Ehsan Haghpanah, 2022.
+# haghpanah@scenus.com
+#
 
-print("OK")
+from helpers import *
+from game import Game
+from game import CheckResult
+
+#
+# main loop
+def void_main() -> None:
+    print("----- pythonic interactive game -----")
+
+    lb = get_int("give me the lower bound = ")
+    ub = get_int("give me the upper bound = ")
+    if lb >= ub:
+        print("the lower bound must be less than the upper bound!")
+        exit()
+
+    print("----- game started -----")
+
+    gm = Game(lb, ub)
+    while True:
+        ky = input(f"give me your guess, an integer between {gm.lower_bound} and {gm.upper_bound}, 'q' to quit = ")
+        if ky == "q":
+            print("game quited, bye!")
+            break
+
+        gi = try_parse(ky)
+        if gi == None:
+            continue
+
+        cs = gm.check(gi)
+        if cs == CheckResult.STOP:
+            print("WOW! your guess is correct. game is over!")
+            break
+        elif cs == CheckResult.LESS:
+            print("not match, GO LOWER!")
+        else:
+            print("not match, GO UPPER!")
+
+#
+# calling main function
+void_main()
