9bacfbbc6213894e624219c2430044e98e20653b
Ehsan Haghpanah <anonim.developer@gmail.com>
2022-07-18 01:28:18 +0430

    .

---
+++ homework-03/test3.py
@@ -0,0 +1,24 @@
+
+
+def foo():
+	return (1, 2)
+print(foo())
+
+a = (2, )
+print(a)
+
+b = []
+if (a):
+	print("not-empty")
+else:
+	print("empty")
+
+d = {'p1':'v1', 'p2':'v2', 'p3':'v3'}
+for x in d:
+	print(x.value())
+
+o = {}
+o['name'] = 'ali'
+o['age'] = 20
+print(o)
+
