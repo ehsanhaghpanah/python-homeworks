38d1daaee18c26eb3d8af13542a7283e5451ec58
Ehsan Haghpanah <anonim.developer@gmail.com>
2022-07-18 02:28:51 +0430

    .

--- homework-03/test3.py
+++ homework-03/test3.py
@@ -1,24 +1,32 @@
 
+# def foo():
+# 	return (1, 2)
+# print(foo())
 
-def foo():
-	return (1, 2)
-print(foo())
+# a = (2, )
+# print(a)
 
-a = (2, )
-print(a)
+# b = []
+# if (a):
+# 	print("not-empty")
+# else:
+# 	print("empty")
 
-b = []
-if (a):
-	print("not-empty")
-else:
-	print("empty")
+# d = {'p1':'v1', 'p2':'v2', 'p3':'v3'}
+# for x in d:
+# 	print(x.value())
 
-d = {'p1':'v1', 'p2':'v2', 'p3':'v3'}
-for x in d:
-	print(x.value())
+# o = {}
+# o['name'] = 'ali'
+# o['age'] = 20
+# print(o)
+# del o['name']
+# print(o)
 
-o = {}
-o['name'] = 'ali'
-o['age'] = 20
-print(o)
+from typing import NewType
 
+UserId = NewType('UserId', int)
+print(type(UserId))
+
+some_id = UserId(524313)
+print(type(some_id))
