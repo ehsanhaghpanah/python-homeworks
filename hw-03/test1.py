df18cdc81617d9188e31ef58f34dc70e97814cc6
Ehsan Haghpanah <anonim.developer@gmail.com>
2022-07-24 22:52:04 +0430

    .

--- hw-03/test1.py
+++ hw-03/test1.py
@@ -6,21 +6,21 @@ from os import listdir
 from os import path
 
 def get_subfolders(pa: str) -> list:
-	return [dr for dr in listdir(pa) if (path.isdir(dr))]
+	return [path.join(pa, dr) for dr in listdir(pa) if (path.isdir(path.join(pa, dr)))]
 
 def get_files(pa: str, ex: str) -> list:
-	return [fi for fi in listdir(pa) if (path.isfile(fi) and fi.endswith(ex))]
+	return [path.join(pa, fi) for fi in listdir(pa) if (path.isfile(path.join(pa, fi)) and fi.endswith(ex))]
 
-def find_files(pa: str, ex: str, ls: list) -> None:
+def find_files(pa: str, ex: str, ls: list) -> list:
 	ls.extend(get_files(pa, ex))
 	ds = get_subfolders(pa)
 	for dr in ds:
 		find_files(dr, ex, ls)
+	return ls
 
+def main_run() -> None:
+	ls = []
+	find_files("f:\z2", ".csv", ls)
+	print(ls)
 
-def foo(ls: list):
-	ls.extend([3, 5])
-
-a = [1, 2, 4]
-foo(a)
-print(a)
+main_run()
